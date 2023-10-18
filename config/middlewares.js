33b07975eec014665981f8607f155547b5418ec6
Bartosz <bartoszweb8@gmail.com>
2023-10-18 23:28:32 +0200

    acces to env and middlewares.js code that mayble block default domain from vps

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,16 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
+module.exports = ({ env }) => ({
+  settings: {
+    cors: {
+      enabled: true,
+      // configure CORS to app;s client side and Strapi client (admin panel)
+      // client-side 1: http://localhost:3000
+      // client-side 2: http://localhost:8000
+      // Strapi client (admin panel): http://localhost:1337
+      origin: ['https://api.genialnastrona.pl'], 
+    },
+  },
+});
